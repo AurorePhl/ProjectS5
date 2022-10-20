46ae1f5ad8ad8807b7161bed3835325251f88098
aurore.philippe <asiphilippe@hotmail.com>
2022-10-20 11:41:59 +0200

    import architecture api rest

---
+++ src/main/java/com/project/quizestce/QuizestceApplication.java
@@ -0,0 +1,13 @@
+package com.project.quizestce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizestceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizestceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/quizestce/QuizestceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.quizestce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizestceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
