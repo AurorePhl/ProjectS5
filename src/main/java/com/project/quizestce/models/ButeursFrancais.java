668229729f99811f6969cecab66f65d272fac6b1
aurore.philippe <asiphilippe@hotmail.com>
2022-11-17 11:15:11 +0100

    getters & setters

--- src/main/java/com/project/quizestce/models/ButeursFrancais.java
+++ src/main/java/com/project/quizestce/models/ButeursFrancais.java
@@ -4,7 +4,46 @@ public class ButeursFrancais {
 	private int id;
 	private String nom;
 	private String prenom;
-	private int age; 
 	private int nbButsMarques;
 	private boolean enActivite;
+	
+	private int age; 
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getNbButsMarques() {
+		return nbButsMarques;
+	}
+	public void setNbButsMarques(int nbButsMarques) {
+		this.nbButsMarques = nbButsMarques;
+	}
+	public boolean isEnActivite() {
+		return enActivite;
+	}
+	public void setEnActivite(boolean enActivite) {
+		this.enActivite = enActivite;
+	}
+
 }
--- src/main/java/com/project/quizestce/models/Questions.java
+++ src/main/java/com/project/quizestce/models/Questions.java
@@ -4,4 +4,25 @@ public class Questions {
 	private String motCle;
 	private String requete;
 	private String question;
+	
+	public String getMotCle() {
+		return motCle;
+	}
+	public void setMotCle(String motCle) {
+		this.motCle = motCle;
+	}
+	public String getRequete() {
+		return requete;
+	}
+	public void setRequete(String requete) {
+		this.requete = requete;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	
+	
 }
